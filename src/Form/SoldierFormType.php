1eeae7d6ec300d550fe921d09facee96316f12da
Roman <raman.yudo@gmail.com>
2020-01-16 21:32:59 +0300

    add forms

---
+++ src/Form/SoldierFormType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\MilitaryUnit;
+use App\Entity\Soldier;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SoldierFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('first_name')
+            ->add('last_name')
+            ->add('third_name')
+            ->add('military_unit', EntityType::class,['class'=>MilitaryUnit::class,'choice_label'=>'name' ])
+            ->add('save', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Soldier::class,
+        ]);
+    }
+}
